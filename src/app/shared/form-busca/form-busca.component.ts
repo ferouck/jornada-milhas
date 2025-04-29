226bbda4f4797e24ce396a9803751cd9ca47997d
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-04-29 14:57:34 -0300

    Abertura e fechamento

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -9,12 +9,5 @@ import { FormBuscaService } from 'src/app/core/services/form-busca.service';
   styleUrls: ['./form-busca.component.scss']
 })
 export class FormBuscaComponent {
-  constructor(public dialog: MatDialog,
-    public formBuscaService: FormBuscaService) {}
-
-  openDialog() {
-    this.dialog.open(ModalComponent, {
-      width: '50%'
-    });
-  }
+  constructor(public formBuscaService: FormBuscaService) {}
 }

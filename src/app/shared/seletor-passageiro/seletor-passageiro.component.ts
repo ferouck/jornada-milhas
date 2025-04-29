4070c93b50ca51d6fda226c60022147d9aa85ee7
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-04-29 15:36:48 -0300

    Implementando o ControlValueAccessor

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,11 +1,30 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-seletor-passageiro',
   templateUrl: './seletor-passageiro.component.html',
   styleUrls: ['./seletor-passageiro.component.scss']
 })
-export class SeletorPassageiroComponent {
+export class SeletorPassageiroComponent implements ControlValueAccessor {
   @Input() titulo: string = '';
   @Input() subtitulo: string = '';
+
+  value: number = 0;
+  onChange = () => {}
+  onTouch = () => {}
+
+  writeValue(val: any): void {
+    this.value = val
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
 }

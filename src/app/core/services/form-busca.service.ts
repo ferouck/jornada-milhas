4f491165c75b90154b5132dd60ad82153b467d62
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-04-29 15:23:07 -0300

    Desafio: descrevendo todos os passageiros

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -16,8 +16,33 @@ export class FormBuscaService {
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
-      tipo: new FormControl('Economica')
+      tipo: new FormControl('Economica'),
+      adultos: new FormControl(1),
+      criancas: new FormControl(0),
+      bebes: new FormControl(0)
     });
+  }
+
+  getDescricaoPassageiros(): string {
+    let descricao = '';
+
+    const adultos = this.formBusca.get('adultos')?.value
+    const criancas = this.formBusca.get('criancas')?.value
+    const bebes = this.formBusca.get('bebes')?.value
+
+    if(adultos && adultos > 0) {
+      descricao += `${adultos} adulto${adultos > 1 ? 's' : ''}`;
+    }
+
+    if(criancas && criancas > 0) {
+      descricao += `${descricao ? ', ' : ''}${criancas} criança${criancas > 1 ? 's' : ''}`;
+    }
+
+    if(bebes && bebes > 0) {
+      descricao += `${descricao ? ', ' : ''}${bebes} bebê${bebes > 1 ? 's' : ''}`;
+    }
+
+    return descricao
   }
 
   obterControle(nome:string): FormControl {

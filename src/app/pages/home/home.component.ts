371f8b8b1f63ad56fcadf1db2b8b0dbd147b8223
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-04-29 09:55:00 -0300

    Desafio: ajustes de layout e novos servicos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +8,16 @@ import { PromocaoService } from 'src/app/core/services/promocao.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  promocoes!: Promocao[];
+
   constructor( private servicoPromocao: PromocaoService) {
 
   }
   ngOnInit(): void {
     this.servicoPromocao.listar()
     .subscribe(
-      resposta => {
-        console.log(resposta)
+      (res) => {
+        this.promocoes = res;
       }
     )
   }

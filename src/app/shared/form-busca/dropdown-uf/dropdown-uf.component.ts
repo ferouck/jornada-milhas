24374b7d12fe49387eaae24d48e3b104f5b19d3d
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-04-29 11:06:04 -0300

    Controles do form

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UnidadeFederativaService } from 'src/app/core/services/unidade-federativa.service';
+import { UnidadeFederativa } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-dropdown-uf',
   templateUrl: './dropdown-uf.component.html',
   styleUrls: ['./dropdown-uf.component.scss']
 })
-export class DropdownUfComponent {
+export class DropdownUfComponent implements OnInit {
   @Input() label: string = '';
   @Input() iconePrefix: string = '';
+  unidadesFederativas: UnidadeFederativa[] = [];
+
   filteredOptions = [];
+
+  constructor(private unidadeFerederativaService: UnidadeFederativaService) {
+
+  }
+  ngOnInit(): void {
+    this.unidadeFerederativaService.listar()
+      .subscribe(dados => {
+        this.unidadesFederativas = dados
+        console.log(this.unidadesFederativas)
+      })
+  }
 }
